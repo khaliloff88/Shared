025a100093a108202d7f219ca53942d099224644
Khalil <khalil.battikh8@gmail.com>
2026-02-01 14:00:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,10 +55,10 @@
 */
 const messages = [
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
+    "Zid Thabet",
+    "Ekher Marra ?",
+    "Yes please..",
+    " behy khamem !",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
